3b04e33b7616d30b46aeae56fdf5f2e9d2344ff6
ahmadalnujaidi <ahmadalnujaidi02@gmail.com>
2024-09-29 11:21:44 +0300

    route params

--- app.js
+++ app.js
@@ -1,57 +1,34 @@
-const http = require("http");
-const { readFileSync } = require("fs");
+const express = require("express");
+const app = express();
+const { products } = require("./data");
 
-// get all files
-const homePage = readFileSync("./navbar-app/index.html");
-const homeStyles = readFileSync("./navbar-app/styles.css");
-const homeImage = readFileSync("./navbar-app/logo.svg");
-const homeLogic = readFileSync("./navbar-app/browser-app.js");
-
-const server = http.createServer((req, res) => {
-  // console.log(req.method);
-  const url = req.url;
-  console.log(req.url);
-  // home page
-  if (url === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(homePage);
-    res.end();
-  }
-
-  // about page
-  else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>About page</h1>");
-    res.end();
-  }
-
-  // styles
-  else if (url === "/styles.css") {
-    res.writeHead(200, { "content-type": "text/css" });
-    res.write(homeStyles);
-    res.end();
-  }
-
-  // Logo
-  else if (url === "/logo.svg") {
-    res.writeHead(200, { "content-type": "image/svg+xml" });
-    res.write(homeImage);
-    res.end();
-  }
-
-  // Logic
-  else if (url === "/browser-app.js") {
-    res.writeHead(200, { "content-type": "text/javascript" });
-    res.write(homeLogic);
-    res.end();
-  }
-
-  // 404
-  else {
-    res.writeHead(404, { "content-type": "text/html" });
-    res.write("<h1>paeg not found</h1>");
-    res.end();
-  }
+app.get("/", (req, res) => {
+  res.send('<h1> Home Page </h1><a href="/api/products">products</a>');
 });
 
-server.listen(5000);
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
+});
+
+app.get("/api/products/:productID", (req, res) => {
+  // console.log(req);
+  // console.log(req.params);
+
+  const { productID } = req.params;
+
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!singleProduct) {
+    return res.status(404).send("product does not exist");
+  }
+  res.json(singleProduct);
+});
+
+app.listen(5000, (req, res) => {
+  console.log("server is running on port 5000...");
+});
