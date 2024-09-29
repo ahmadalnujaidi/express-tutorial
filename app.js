a8d8af397bd3e874245b3cd173cfc80e00bedba4
ahmadalnujaidi <ahmadalnujaidi02@gmail.com>
2024-09-29 13:58:04 +0300

    query string

--- app.js
+++ app.js
@@ -29,6 +29,32 @@ app.get("/api/products/:productID", (req, res) => {
   res.json(singleProduct);
 });
 
+app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
+  console.log(req.params);
+  res.send("hello world");
+});
+
+app.get("/api/v1/query", (req, res) => {
+  // console.log(req.query);
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+  if (sortedProducts.length < 1) {
+    // res.status(200).send("no products matched your search");
+    return res.status(200).json({ success: true, data: [] });
+  }
+  res.status(200).json(sortedProducts);
+  // res.send("hello world");
+});
+
 app.listen(5000, (req, res) => {
   console.log("server is running on port 5000...");
 });
