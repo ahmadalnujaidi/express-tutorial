2828ede5983f77aa798fec1e102128150912851b
ahmadalnujaidi <ahmadalnujaidi02@gmail.com>
2024-09-30 10:47:17 +0300

    methods - post (form example)

--- app.js
+++ app.js
@@ -1,60 +1,25 @@
 const express = require("express");
 const app = express();
-const { products } = require("./data");
+let { people } = require("./data");
 
-app.get("/", (req, res) => {
-  res.send('<h1> Home Page </h1><a href="/api/products">products</a>');
+// static assets
+app.use(express.static("./methods-public"));
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
 });
 
-app.get("/api/products", (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
-  res.json(newProducts);
-});
-
-app.get("/api/products/:productID", (req, res) => {
-  // console.log(req);
-  // console.log(req.params);
-
-  const { productID } = req.params;
-
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  );
-  if (!singleProduct) {
-    return res.status(404).send("product does not exist");
+app.post("/login", (req, res) => {
+  // console.log(req.body);
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
   }
-  res.json(singleProduct);
+  res.status(401).send("please provide credentials");
 });
 
-app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
-  console.log(req.params);
-  res.send("hello world");
-});
-
-app.get("/api/v1/query", (req, res) => {
-  // console.log(req.query);
-  const { search, limit } = req.query;
-  let sortedProducts = [...products];
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search);
-    });
-  }
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit));
-  }
-  if (sortedProducts.length < 1) {
-    // res.status(200).send("no products matched your search");
-    return res.status(200).json({ success: true, data: [] });
-  }
-  res.status(200).json(sortedProducts);
-  // res.send("hello world");
-});
-
-app.listen(5000, (req, res) => {
-  console.log("server is running on port 5000...");
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
 });
