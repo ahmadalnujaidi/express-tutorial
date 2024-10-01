928a75f2b502f47159c2e370ffb29371910c2c2d
ahmadalnujaidi <ahmadalnujaidi02@gmail.com>
2024-10-01 10:10:53 +0300

    express tutorial complete

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
 const express = require("express");
 const app = express();
-let { people } = require("./data");
+
+const people = require("./routes/people");
+const auth = require("./routes/auth");
 
 // static assets
 app.use(express.static("./methods-public"));
 // parse form data
 app.use(express.urlencoded({ extended: false }));
+// parse json
+app.use(express.json());
 
-app.get("/api/people", (req, res) => {
-  res.status(200).json({ success: true, data: people });
-});
-
-app.post("/login", (req, res) => {
-  // console.log(req.body);
-  const { name } = req.body;
-  if (name) {
-    return res.status(200).send(`Welcome ${name}`);
-  }
-  res.status(401).send("please provide credentials");
-});
+app.use("/api/people", people);
+app.use("/login", auth);
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
